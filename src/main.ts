f48e2fe913c43460d223d730e6d3a55aeacd7bf1
Prince Narteh <prinarteh@gmail.com>
2023-06-05 11:44:05 +0000

    fix errors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { ValidationPipe, ForbiddenException } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
 import { join } from 'path';
 
-const whitelist = [
-  'https://mimallgh.com',
-  'https://admin.mimallgh.com',
-  'https://shop.mimallgh.com',
-];
+// const whitelist = [
+//   'https://mimallgh.com',
+//   'https://admin.mimallgh.com',
+//   'https://shop.mimallgh.com',
+// ];
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -21,14 +21,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new ForbiddenException('Not allowed by CORS'));
-      }
-    },
-      
+    origin: '*',
   });
   app.useStaticAssets(join(__dirname, '../uploads'));
   await app.listen(parseInt(process.env.PORT) || 4000);
