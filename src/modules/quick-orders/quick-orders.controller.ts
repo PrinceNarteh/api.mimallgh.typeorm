e3b816ead900f4322d825dfdf5d0b3d32985fb8f
Prince Narteh <prinarteh@gmail.com>
2023-06-30 05:07:17 +0000

    add create and get all quick orders controller and service handlers

--- src/modules/quick-orders/quick-orders.controller.ts
+++ src/modules/quick-orders/quick-orders.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body } from '@nestjs/common';
+import { QuickOrdersService } from './quick-orders.service';
+import { CreateQuickOrderDto } from './dto/quickOrderDto';
 
 @Controller('quick-orders')
-export class QuickOrdersController {}
+export class QuickOrdersController {
+  constructor(private readonly orderService: QuickOrdersService) {}
+
+  @Get()
+  async getAllQuickOrders(@Param() param: { [key: string]: string }) {
+    return this.orderService.getAllQuickOrders(param);
+  }
+
+  @Post()
+  async createQuickOrder(@Body() order: CreateQuickOrderDto) {
+    return this.orderService.createQuickOrder(order);
+  }
+}
--- src/modules/quick-orders/quick-orders.service.ts
+++ src/modules/quick-orders/quick-orders.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QuickOrder } from 'src/entities/quick-order.entity';
+import { Repository } from 'typeorm';
+import { CreateQuickOrderDto } from './dto/quickOrderDto';
 
 @Injectable()
-export class QuickOrdersService {}
+export class QuickOrdersService {
+  constructor(
+    @InjectRepository(QuickOrder)
+    private quickOrderRepo: Repository<QuickOrder>,
+  ) {}
+
+  async getAllQuickOrders(param: { [key: string]: string }) {
+    const builder = this.quickOrderRepo.createQueryBuilder('order');
+
+    // pagination
+    const page = parseInt(param.page, 10) || 1;
+    const perPage = parseInt(param.perPage, 10) || 10;
+    const total = await builder.getCount();
+
+    builder.offset(perPage * (page - 1)).limit(perPage);
+
+    return {
+      data: await builder.getMany(),
+      total,
+      page,
+      lastPage: Math.ceil(total / perPage),
+    };
+  }
+
+  async createQuickOrder(order: CreateQuickOrderDto) {
+    const quickOrder = this.quickOrderRepo.create(order);
+    await this.quickOrderRepo.save(quickOrder);
+    return quickOrder;
+  }
+}
