5abe9ac82433ecdf50490b28fb8da2ff1d2f7db6
Prince Narteh <prinarteh@gmail.com>
2023-09-26 19:14:31 +0000

    fix: QuickOrders errors

--- src/modules/quick-orders/quick-orders.service.ts
+++ src/modules/quick-orders/quick-orders.service.ts
@@ -1,14 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { QuickOrder } from 'src/entities/quickOrder.entity';
-import { Repository } from 'typeorm';
 import { DeliveryCompaniesService } from '../delivery-companies/delivery-companies.service';
-import { CreateQuickOrderDto } from './dto/quickOrderDto';
-import { QuickOrderItem } from 'src/entities/QuickOrderItem.entity';
-import { ShopService } from '../shops/shop.service';
 import { ProductService } from '../products/product.service';
-import { QuickOrderRepository } from './quick-orders.repository';
+import { ShopService } from '../shops/shop.service';
+import { CreateQuickOrderDto } from './dto/quickOrderDto';
 import { QuickOrderItemRepository } from './quick-order-item.repository';
+import { QuickOrderRepository } from './quick-orders.repository';
 import { QuickOrderDocument } from './schema/quick-order.schema';
 
 @Injectable()
@@ -22,21 +18,7 @@ export class QuickOrdersService {
   ) {}
 
   async getAllQuickOrders(param: { [key: string]: string }) {
-    const builder = this.quickOrderRepo.createQueryBuilder('order');
-
-    // pagination
-    const page = parseInt(param.page, 10) || 1;
-    const perPage = parseInt(param.perPage, 10) || 10;
-    const total = await builder.getCount();
-
-    builder.offset(perPage * (page - 1)).limit(perPage);
-
-    return {
-      data: await builder.getMany(),
-      total,
-      page,
-      lastPage: Math.ceil(total / perPage),
-    };
+    return await this.quickOrderRepo.find({});
   }
 
   async getQuickOrder(
@@ -68,7 +50,7 @@ export class QuickOrdersService {
       ),
     });
 
-    await this.quickOrderRepo.save(quickOrder);
+    await this.quickOrderRepo.create(quickOrder);
 
     for (const item of items) {
       const shop = await this.shopService.shop(item.shopId);
@@ -79,8 +61,6 @@ export class QuickOrdersService {
         product,
         order: quickOrder,
       });
-
-      await res.save();
     }
 
     return quickOrder;
