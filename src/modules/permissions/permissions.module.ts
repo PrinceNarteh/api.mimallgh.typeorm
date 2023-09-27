84d28fed5ab095a0743925bfb0f986a0ea6c2cca
Prince Narteh <prinarteh@gmail.com>
2023-09-27 00:30:33 +0000

    fix errors

--- src/modules/permissions/permissions.module.ts
+++ src/modules/permissions/permissions.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsController } from './permissions.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PERMISSION_MODEL,
+  PermissionSchema,
+} from './schema/permissions.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: PERMISSION_MODEL,
+        schema: PermissionSchema,
+      },
+    ]),
+  ],
   providers: [PermissionsService],
-  controllers: [PermissionsController]
+  controllers: [PermissionsController],
 })
 export class PermissionsModule {}
--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
+import { ROLE_MODEL, RoleSchema } from './schema/role.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: ROLE_MODEL, schema: RoleSchema }]),
+  ],
   controllers: [RolesController],
-  providers: [RolesService]
+  providers: [RolesService],
 })
 export class RolesModule {}
